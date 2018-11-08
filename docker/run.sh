3317647f9714536fdac8acae1d608a5bec99e028
Dmitry Shlagoff <wessmoke@mail.ru>
2018-11-08 14:31:38 +0700

    Remove secure websockets setup

--- docker/run.sh
+++ docker/run.sh
@@ -7,17 +7,13 @@ PLUGIN="libjanus_conference.so"
 DOCKER_CONTAINER_NAME="sandbox/${PROJECT}"
 DOCKER_CONTAINER_COMMAND=${DOCKER_CONTAINER_COMMAND:-'/bin/bash'}
 DOCKER_RUN_OPTIONS=${DOCKER_RUN_OPTIONS:-'-ti --rm'}
-DOCKER_WSS_PORT=${DOCKER_WSS_PORT:-'8989'}
 DOCKER_WS_PORT=${DOCKER_WS_PORT:-'8188'}
-WS_CONFIG="${JANUS_DIR}/etc/janus/janus.transport.websockets.cfg"
 
 read -r DOCKER_RUN_COMMAND <<-EOF
-    perl -pi -e 's/(wss = )no/\${1}yes/' "${WS_CONFIG}" \
-    && perl -pi -e 's/;(wss_port = 8989)/\${1}/' "${WS_CONFIG}" \
-    && cargo build --release \
+    cargo build --release \
     && (rm "${JANUS_DIR}/lib/janus/plugins/${PLUGIN}" || true) \
     && ln -s "${PROJECT_DIR}/target/release/${PLUGIN}" "${JANUS_DIR}/lib/janus/plugins/${PLUGIN}" \
-    && ${JANUS_DIR}/bin/janus --token-auth --token-auth-secret=xepohgue3Wa3yohg0aiP --event-handlers --debug-level=6
+    && ${JANUS_DIR}/bin/janus --event-handlers --debug-level=6
 EOF
 
 set -ex
@@ -30,7 +26,6 @@ docker build -t ${DOCKER_CONTAINER_NAME} -f docker/Dockerfile .
 docker run ${DOCKER_RUN_OPTIONS} \
     -v $(pwd):${PROJECT_DIR} \
     -v ${BUILD_CACHE_VOLUME}:/usr/local/cargo \
-    -p ${DOCKER_WSS_PORT}:8989 \
     -p ${DOCKER_WS_PORT}:8188 \
     -e "DOCKER_RUN_COMMAND=${DOCKER_RUN_COMMAND}" \
     ${DOCKER_CONTAINER_NAME} \
