5a792cf9c87b1386c91078579899d7529bc0bf5e
Timofey Martynov <feymartynov@users.noreply.github.com>
2021-02-09 21:08:18 +0300

    Fix exploit in signal.update (#109)

--- src/message_handler/operations/signal_update.rs
+++ src/message_handler/operations/signal_update.rs
@@ -3,8 +3,6 @@ use async_trait::async_trait;
 use http::StatusCode;
 use svc_error::Error as SvcError;
 
-use crate::jsep::Jsep;
-
 #[derive(Clone, Debug, Deserialize)]
 pub struct Request {}
 
@@ -18,36 +16,21 @@ impl super::Operation for Request {
 
         let error = |status: StatusCode, err: Error| {
             SvcError::builder()
-                .kind("stream_read_error", "Error reading a stream")
+                .kind("signal_update_error", "Error updating signal")
                 .status(status)
                 .detail(&err.to_string())
                 .build()
         };
-
-        let jsep_offer = request
-            .jsep_offer()
-            .ok_or_else(|| error(StatusCode::BAD_REQUEST, anyhow!("Missing JSEP")))?;
-
-        let will_be_writer =
-            Jsep::is_writer(jsep_offer).map_err(|err| error(StatusCode::BAD_REQUEST, err))?;
 
         let app = app!().map_err(|err| error(StatusCode::INTERNAL_SERVER_ERROR, err))?;
         let session_id = request.session_id().to_owned();
 
         app.switchboard_dispatcher
             .dispatch(move |switchboard| -> anyhow::Result<()> {
-                if will_be_writer {
-                    // Reader becomes writer.
-                    if let Some(stream_id) = switchboard.read_by(session_id) {
-                        switchboard.remove_reader(stream_id, session_id);
-                        switchboard.set_writer(stream_id, session_id)?;
-                    }
-                } else {
-                    // Writer becomes reader.
-                    if let Some(stream_id) = switchboard.written_by(session_id) {
-                        switchboard.remove_writer(stream_id)?;
-                        switchboard.add_reader(stream_id, session_id);
-                    }
+                if let Some(stream_id) = switchboard.read_by(session_id) {
+                    switchboard.remove_reader(stream_id, session_id);
+                } else if let Some(stream_id) = switchboard.written_by(session_id) {
+                    switchboard.remove_writer(stream_id)?;
                 }
 
                 Ok(())
