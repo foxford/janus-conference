c2b24244700dabb5154e4533ab8d978043be9aa3
Ilya Titkov <ilya@titkov.me>
2021-08-09 15:16:14 +0300

    newline

--- src/message_handler/operations/mod.rs
+++ src/message_handler/operations/mod.rs
@@ -2,8 +2,8 @@ pub use super::{Operation, OperationResult, Request};
 
 pub mod agent_leave;
 pub mod reader_config_update;
+pub mod service_ping;
 pub mod stream_create;
 pub mod stream_read;
 pub mod stream_upload;
 pub mod writer_config_update;
-pub mod service_ping;
