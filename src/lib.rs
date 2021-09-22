cbb069027a799927ac1c10be21bae9bf56ab3fcc
Ilya Titkov <ilya@titkov.me>
2021-09-22 13:41:12 +0300

    Clippy

--- src/lib.rs
+++ src/lib.rs
@@ -325,7 +325,7 @@ extern "C" fn data_ready(_handle: *mut PluginSession) {
     // Skip data channels.
 }
 
-extern "C" fn slow_link(handle: *mut PluginSession, uplink: c_int, video: c_int) {}
+extern "C" fn slow_link(_handle: *mut PluginSession, _uplink: c_int, _video: c_int) {}
 
 extern "C" fn hangup_media(handle: *mut PluginSession) {
     report_error(hangup_media_impl(handle));
