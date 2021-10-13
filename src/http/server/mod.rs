d8031e69e0fe850ed06ce048daf0bc9e0a936ca2
Ilya Titkov <ilya@titkov.me>
2021-10-13 17:32:44 +0300

    Fixing query params

--- src/http/server/mod.rs
+++ src/http/server/mod.rs
@@ -44,7 +44,8 @@ fn map_result<T>(
 
 #[derive(Deserialize)]
 struct MaxEvents {
-    max_events: usize,
+    #[serde(default)]
+    max_events: Option<usize>,
 }
 
 pub fn router(janus_client: JanusClient) -> Router<BoxRoute> {
@@ -70,11 +71,11 @@ pub fn router(janus_client: JanusClient) -> Router<BoxRoute> {
             "/poll",
             get(
                 |janus_client: Extension<Arc<JanusClient>>,
-                 max_events: Option<Query<MaxEvents>>| async move {
+                 max_events: Query<MaxEvents>| async move {
                     map_result(
                         timeout(
                             Duration::from_secs(30),
-                            janus_client.get_events(max_events.map(|x| x.max_events).unwrap_or(5)),
+                            janus_client.get_events(max_events.max_events.unwrap_or(5)),
                         )
                         .await
                         .unwrap_or_else(|_| Ok(Vec::new())),
