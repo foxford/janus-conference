1d2c6ed9959af30d477581902a9d3c438dba1911
Ilya Titkov <ilya@titkov.me>
2021-10-14 17:28:22 +0300

    Fix debug

--- src/http/client/mod.rs
+++ src/http/client/mod.rs
@@ -275,7 +275,7 @@ async fn polling(
             Ok(events) => {
                 for event in events {
                     if let Some(event_kind) = event.get("janus").and_then(|x| x.as_str()) {
-                        info!("Got event: {}", event_kind)?;
+                        info!("Got event: {}", event_kind);
                         if skip_events.iter().any(|e| e.as_str() == event_kind) {
                             continue;
                         }
